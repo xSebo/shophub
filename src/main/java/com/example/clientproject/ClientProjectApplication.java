8fe8216c92ca22426460ae27083f6c3925ff5f6e
c2038058 <gillj8@cardiff.ac.uk>
2021-11-11 17:31:49 +0000

    Initial upload of empty Spring Project

---
+++ src/main/java/com/example/clientproject/ClientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientproject/ClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
