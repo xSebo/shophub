52ad502308ea9893d3cbde35bed14e07b61a9565
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-16 11:03:52 +0000

    Added controller for index and example static file linking

---
+++ src/main/java/com/example/clientproject/web/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.clientproject.web.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping
+    public String index(Model model){
+        return "index";
+    }
+}
