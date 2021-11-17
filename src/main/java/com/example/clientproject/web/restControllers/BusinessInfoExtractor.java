857d77eec285ab37f43cc135a4cea437bcf87662
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-17 12:39:30 +0000

    folder refactor

--- src/main/java/com/example/clientproject/web/restControllers/BusinessInfoExtractor.java
+++ src/main/java/com/example/clientproject/web/restControllers/BusinessInfoExtractor.java
@@ -1,4 +1,4 @@
-package com.example.clientproject.web.controllers;
+package com.example.clientproject.web.restControllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
