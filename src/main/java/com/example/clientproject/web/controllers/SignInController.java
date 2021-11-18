773f975e1e54835bf1c0abd252e505a664ce5474
c2065864 <barnards3@cardiff.ac.uk>
2021-11-18 14:49:11 +0000

    Created post controller, and object mapping for form object

--- src/main/java/com/example/clientproject/web/controllers/SignInController.java
+++ src/main/java/com/example/clientproject/web/controllers/SignInController.java
@@ -1,14 +1,22 @@
 package com.example.clientproject.web.controllers;
 
+import com.example.clientproject.web.forms.BusinessRegisterForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @Controller
 public class SignInController {
+
+    @PostMapping("/businessRegister")
+    public String submitBusinessInfo(BusinessRegisterForm brf){
+        System.out.println(brf.getBusinessTags());
+        return "registerbusiness.html";
+    }
 
     @GetMapping("/businessRegister")
     public String registerBusiness(Model model){
---
+++ src/main/java/com/example/clientproject/web/forms/BusinessRegisterForm.java
@@ -0,0 +1,29 @@
+package com.example.clientproject.web.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BusinessRegisterForm {
+    String business_register_url;
+    String business_register_name;
+    String business_register_desc;
+    String businessCategory;
+    ArrayList<String> businessTags;
+    String instagram;
+    String facebook;
+    String twitter;
+    String tiktok;
+    int earnings;
+
+    public void setTags(String tempTags){
+        this.businessTags = new ArrayList(Arrays.asList(tempTags.split(",")));
+    }
+
+}
