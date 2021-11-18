7ac5ad98599632db2b0b8cea2b04b5ea7d3ff3dd
c2038058 <gillj8@cardiff.ac.uk>
2021-11-18 14:20:40 +0000

    Push so Carl can help with issues

--- src/main/java/com/example/clientproject/data/users/UsersRepo.java
+++ src/main/java/com/example/clientproject/data/users/UsersRepo.java
@@ -1,6 +1,7 @@
 package com.example.clientproject.data.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/example/clientproject/web/controllers/SignInController.java
+++ src/main/java/com/example/clientproject/web/controllers/SignInController.java
@@ -1,19 +1,104 @@
 package com.example.clientproject.web.controllers;
 
+import com.example.clientproject.service.dtos.UsersDTO;
+import com.example.clientproject.service.searches.UsersSearch;
+import com.example.clientproject.web.forms.LoginForm;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.security.SecureRandom;
+import java.util.*;
 
 @Controller
 public class SignInController {
+    public static boolean loggedIn = false;
+
+    private UsersSearch usersSearch;
+
+    public SignInController(UsersSearch aUsersSearch) {
+        usersSearch = aUsersSearch;
+    }
 
     @GetMapping("/register")
     public String registerBusiness(Model model){
         ArrayList<String> categories = new ArrayList<>(Arrays.asList("Food and drink","Animals","Alcohol"));
         model.addAttribute("categories", categories);
+        model.addAttribute("loggedIn", loggedIn);
         return "registerbusiness.html";
+    }
+
+    /**
+     * Method for accessing the login page
+     * An attribute is assigned to the model called "loggedIn", which is false by default as all members accessing this page will not be logged in by this point
+     * @param model - Model object which will be populated and passed into the view
+     * @return - the page to redirect to
+     */
+    @GetMapping("/login")
+    public String loginPageAccess(Model model) {
+        LoginForm loginForm = new LoginForm();
+        model.addAttribute("loginForm", loginForm);
+        model.addAttribute("loggedIn", loggedIn);
+        return "account-login.html";
+    }
+
+    @PostMapping("/login")
+    public String signInChecks(@Valid LoginForm loginForm,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("loggedIn", loggedIn);
+            return "account-login.html";
+        }
+
+        Optional<UsersDTO> usersDTOOptional = usersSearch.findByEmail(loginForm.getLoginEmail());
+        // If the optional is present - the search found a user with that email
+        if (usersDTOOptional.isPresent()) {
+            Random random = new SecureRandom();
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+            // Credits to user "Assylias" https://stackoverflow.com/questions/18142745/how-do-i-generate-a-salt-in-java-for-salted-hash
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+            // For testing, removed random salt generator
+            // String generatedSalt = Base64.getEncoder().encodeToString(salt);
+
+            // Check the password given (after encoding) and the user's DB password match
+            boolean passwordMatch = usersDTOOptional
+                    .get()
+                    .getUserPassword()
+                    .equals(
+                            passwordEncoder.encode(
+                                    loginForm
+                                            // Commented for testing purposes
+                                            //.getLoginPassword() + generatedSalt
+                                            .getLoginPassword() + "EXAMPLESALT"
+                            )
+                    );
+
+            // If they match, set the loggedIn flag to true
+            if (passwordMatch) {
+                loggedIn = true;
+            // Otherwise, redirect back to the login page with an appropriate error
+            } else {
+                model.addAttribute("incorrectPassword", true);
+                model.addAttribute("loggedIn", loggedIn);
+                return "account-login.html";
+            }
+        // Else - assumes that the email is incorrect
+        } else {
+            model.addAttribute("incorrectEmail", true);
+            model.addAttribute("loggedIn", loggedIn);
+            return "account-login.html";
+        }
+
+        model.addAttribute("loggedIn", loggedIn);
+        return "index.html";
     }
 }
