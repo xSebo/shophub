bbaaf1e7d348df199edf722137b7e61120f9769a
c2064724 <allen-harrise@cardiff.ac.uk>
2021-11-20 12:36:33 +0000

    Changed SignUp method to use service, and started category selection after signup
    Signed-off-by: c2064724 <allen-harrise@cardiff.ac.uk>

---
+++ src/main/java/com/example/clientproject/services/registerUserServiceStatic.java
@@ -0,0 +1,33 @@
+package com.example.clientproject.services;
+
+import com.example.clientproject.data.twoFactorMethods.TwoFactorMethods;
+import com.example.clientproject.data.twoFactorMethods.TwoFactorMethodsRepo;
+import com.example.clientproject.data.users.Users;
+import com.example.clientproject.data.users.UsersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class registerUserServiceStatic implements registerUserService{
+    @Autowired
+    UsersRepo usersRepo;
+    @Autowired
+    TwoFactorMethodsRepo twoFactorMethodsRepo;
+
+    public void save(newAccountDTO accountDTO){
+        long ID = 1;
+        TwoFactorMethods twoFactorMethods = new TwoFactorMethods();
+        twoFactorMethods.setTwoFactorMethodId(ID);
+        twoFactorMethods.setTwoFactorMethodName("None");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Users newUser = new Users(accountDTO.getName(), accountDTO.getSurname(), accountDTO.getEmail(), accountDTO.getPassword(),
+                "", "",
+                LocalDateTime.now().format(formatter), twoFactorMethods);
+        Users users = usersRepo.save(newUser);
+
+    }
+}
