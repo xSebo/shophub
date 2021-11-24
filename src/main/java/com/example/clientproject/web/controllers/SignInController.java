d80f31516955f716a5f09cd6c3be2ee58fe2b0f8
c2038058 <gillj8@cardiff.ac.uk>
2021-11-24 17:52:41 +0000

    Password Management Fixed

--- src/main/java/com/example/clientproject/web/controllers/SignInController.java
+++ src/main/java/com/example/clientproject/web/controllers/SignInController.java
@@ -9,6 +9,8 @@ import com.example.clientproject.web.forms.BusinessRegisterForm;
 import com.example.clientproject.web.forms.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
