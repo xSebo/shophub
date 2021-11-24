307594b7044632e700ac570c1233d352927af85a
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-24 11:38:22 +0000

    added example session routes

---
+++ src/main/java/com/example/clientproject/web/controllers/SessionTestController.java
@@ -0,0 +1,38 @@
+package com.example.clientproject.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SessionTestController {
+    @GetMapping("/session")
+    public String getSessionPage(Model model, HttpSession session){
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+        model.addAttribute("sessionData",messages);
+        return "session-test";
+    }
+
+    @GetMapping("/sessionSave")
+    public String setSessionData(Model model, HttpSession session){
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            session.setAttribute("MY_SESSION_MESSAGES", messages);
+        }
+        messages.add("test");
+        session.setAttribute("MY_SESSION_MESSAGES", messages);
+
+        model.addAttribute("sessionData",messages);
+        return "session-test";
+    }
+}
+
+
+
+
