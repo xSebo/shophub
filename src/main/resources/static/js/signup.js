946254bc9d4fa1e1a51a7fc5e1fb4dac329dce32
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-26 15:50:41 +0000

    made email checking async so it would wait to see if the email is valid

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,4 +1,4 @@
-function validateSignup(){
+async function validateSignup(){
     let form = document.forms["signUpForm"]
 
     let valid = true;
@@ -49,6 +49,29 @@ function validateSignup(){
         }
     }
 
+    let passwordField = form["newUserPassword"]
+    let passwordHelpText = document.getElementById("passwordHelpText")
+    if(passwordField.value == ""){
+        passwordField.classList.remove("is-success")
+        passwordField.classList.add("is-danger")
+        passwordHelpText.innerText = "Required"
+        passwordHelpText.style.display = "block"
+        valid = false;
+    }else{
+        if(passwordField.value.length < 7 || passwordField.value.length > 15){
+            passwordField.classList.remove("is-success")
+            passwordField.classList.add("is-danger")
+            passwordHelpText.innerText = "Must be between 7 and 15 characters"
+            passwordHelpText.style.display = "block"
+            valid = false;
+        }else{
+            passwordField.classList.add("is-success")
+            passwordField.classList.remove("is-danger")
+            passwordHelpText.innerText = ""
+            passwordHelpText.style.display = "none"
+        }
+    }
+
     var mail_format = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])";
     const regex = new RegExp(mail_format);
     let emailField = form["newUserEmail"]
@@ -82,34 +105,11 @@ function validateSignup(){
                         emailHelpText.innerText = ""
                         emailHelpText.style.display = "none"
                     }
+                    if(valid){
+                        form.submit()
+                    }
                 })
         }
     }
 
-
-
-    let passwordField = form["newUserPassword"]
-    let passwordHelpText = document.getElementById("passwordHelpText")
-    if(passwordField.value == ""){
-        passwordField.classList.remove("is-success")
-        passwordField.classList.add("is-danger")
-        passwordHelpText.innerText = "Required"
-        passwordHelpText.style.display = "block"
-        valid = false;
-    }else{
-        if(passwordField.value.length < 7 || passwordField.value.length > 15){
-            passwordField.classList.remove("is-success")
-            passwordField.classList.add("is-danger")
-            passwordHelpText.innerText = "Must be between 7 and 15 characters"
-            passwordHelpText.style.display = "block"
-            valid = false;
-        }else{
-            passwordField.classList.add("is-success")
-            passwordField.classList.remove("is-danger")
-            passwordHelpText.innerText = ""
-            passwordHelpText.style.display = "none"
-        }
-    }
-
-    return valid
 }
