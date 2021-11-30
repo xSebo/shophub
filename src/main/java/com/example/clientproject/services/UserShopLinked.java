dc1090121b857bb4e140f4e8d9dad308d30e4a0c
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-30 15:42:55 +0000

    Changed way we handle permission levels on admin page

--- src/main/java/com/example/clientproject/services/UserShopLinked.java
+++ src/main/java/com/example/clientproject/services/UserShopLinked.java
@@ -1,15 +1,25 @@
 package com.example.clientproject.services;
 
 import com.example.clientproject.data.adminTypes.AdminTypesRepo;
+import com.example.clientproject.data.shops.Shops;
 import com.example.clientproject.data.userPermissions.UserPermissionsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class UserShopLinked {
 
     @Autowired
     UserPermissionsRepo userPermissionsRepo;
+
+    @Autowired
+    JdbcTemplate jdbc;
 
     public UserShopLinked(UserPermissionsRepo aUserPermissionsRepo){
         userPermissionsRepo = aUserPermissionsRepo;
@@ -26,5 +36,18 @@ public class UserShopLinked {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public List<Integer> getByUserId(Long userId){
+        String query = "SELECT s.Shop_Id FROM User_Permissions s WHERE (User_Id = " +userId.toString() + ")";
+
+        List<Map<String, Object>> rs = jdbc.queryForList(query);
+        List<Integer> shops = new ArrayList<Integer>();
+
+        for(Map<String, Object> shopLink : rs){
+            shops.add((Integer) shopLink.get("Shop_Id"));
+        }
+
+        return shops;
     }
 }
--- src/main/java/com/example/clientproject/web/controllers/AdminController.java
+++ src/main/java/com/example/clientproject/web/controllers/AdminController.java
@@ -1,5 +1,8 @@
 package com.example.clientproject.web.controllers;
 
+import com.example.clientproject.data.shops.Shops;
+import com.example.clientproject.data.shops.ShopsRepo;
+import com.example.clientproject.data.userPermissions.UserPermissions;
 import com.example.clientproject.data.userPermissions.UserPermissionsRepo;
 import com.example.clientproject.data.users.Users;
 import com.example.clientproject.service.Utils.JWTUtils;
@@ -9,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -16,11 +20,13 @@ public class AdminController {
     public JWTUtils jwtUtils;
     public UserShopLinked userShopLinked;
     public UserPermissionsRepo userPermissionsRepo;
+    public ShopsRepo shopsRepo;
 
-    public AdminController(JWTUtils jwt, UserShopLinked usl, UserPermissionsRepo upr){
+    public AdminController(JWTUtils jwt, UserShopLinked usl, UserPermissionsRepo upr, ShopsRepo sr){
         jwtUtils = jwt;
         userShopLinked = usl;
         userPermissionsRepo = upr;
+        shopsRepo = sr;
     }
 
     @GetMapping("/settings")
@@ -31,9 +37,29 @@ public class AdminController {
             return "redirect:/login";
         }
 
-        boolean userHasShop = userShopLinked.hasShop(jwtUtils.getLoggedInUserId(session).get());
+        //Get the highest permission level of user
+        Long highestPerm = 1L;
+        List<UserPermissions> perms =  userPermissionsRepo.findByUserId(user.get().getUserId());
+        for (UserPermissions perm : perms)
+        {
+            Long newPerm = perm.getAdminType().getAdminTypeId();
+            if(newPerm>highestPerm){
+                highestPerm=newPerm;
+            }
+        }
 
-        model.addAttribute("userHasShop", userHasShop);
+        //Get Shops the user is associated with
+        if(highestPerm > 1){
+            List<Integer> shops = userShopLinked.getByUserId(user.get().getUserId());
+            //Check if user has defined a shop to look at in the url
+
+            //Else choose the first one
+            Shops shop = shopsRepo.getById(Long.valueOf(shops.get(0)));
+
+            model.addAttribute("shop",shop);
+        }
+
+        model.addAttribute("highestPerm", highestPerm);
         model.addAttribute("loggedInUser", user.get());
 
         return "admin";
