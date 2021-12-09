c879bf653bd5472e4e2dda7eec1d94af66d01b55
John Watkins <watkinsj18@cardiff.ac.uk>
2021-12-09 14:49:40 +0000

    Users can add tags and the search query is sent to the correct route with query filter added

--- src/main/java/com/example/clientproject/web/restControllers/ShopSearch.java
+++ src/main/java/com/example/clientproject/web/restControllers/ShopSearch.java
@@ -1,22 +1,45 @@
 package com.example.clientproject.web.restControllers;
 
+import com.example.clientproject.data.shops.Shops;
+import com.example.clientproject.data.shops.ShopsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @RestController
 public class ShopSearch {
+    @Autowired
+    ShopsRepo shopsRepo;
+
     @GetMapping("/shop/search")
     public String searchShops(@RequestParam(value = "q", required = false) String query,
                               @RequestParam(value = "p", required = false) Integer page,
-                              @RequestParam(value = "t", required = false) List<Integer> tags){
-        System.out.println(tags);
-        System.out.println(query);
-        System.out.println(page);
+                              @RequestParam(value = "t", required = false) List<String> tags){
+        final Integer itemsPerPage = 6;
 
-        return "here";
+        //Get all the active shops
+        List<Shops> allShops = shopsRepo.findActiveShops();
+
+        //Filter the shops using the query provided
+        if(query != null){
+            allShops = allShops
+                    .stream()
+                    .filter(s -> s.getShopName().toLowerCase(Locale.ROOT).strip().contains(query.toLowerCase(Locale.ROOT).strip()))
+                    .collect(Collectors.toList());
+        }
+
+        //Filter using the tags provided
+
+        //Paginate
+
+        //Convert to required format
+
+        return allShops.toString();
 
     }
 }
---
+++ src/main/resources/static/js/searchBar.js
@@ -0,0 +1,57 @@
+var tags = [];
+var query = "";
+var page = 1;
+
+var searchBar = document.getElementById("main-search");
+var tagGroup = document.getElementById("search-tag-group");
+
+function updateSearch(e){
+    page = 1;
+    query = searchBar.value;
+    doSearch()
+}
+
+function removeTag(i){
+    tags.splice(i, 1);
+    page=1;
+    updateUI()
+    doSearch()
+}
+
+function addTag(e){
+    if(e.key== "Enter"){
+        if (searchBar.value != ""){
+            page = 1;
+            query = "";
+            tags.push(searchBar.value.toLowerCase());
+            searchBar.value = "";
+            updateUI();
+            doSearch();
+        }
+    }
+}
+
+function updateUI(){
+    tagGroup.innerHTML = "";
+    for(let [i,tag] of tags.entries()){
+        tagGroup.innerHTML += `
+            <div class="control mr-3">
+                <div class="tags has-addons">
+                    <span class="tag gradient">${tag}</span>
+                    <a class="tag is-delete" onclick="removeTag(${i})"></a>
+                </div>
+            </div>`
+    }
+}
+
+function doSearch(){
+    let url = "/shop/search"
+    url += "?q=" + query
+    url += "&p=" + page.toString()
+    for (let t of tags){
+        url += "&t=" + t.toString()
+    }
+    fetch(url)
+        .then(e=>e.text())
+        .then(data=>console.log(data))
+}
