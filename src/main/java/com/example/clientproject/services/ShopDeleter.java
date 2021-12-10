aa97c86cd5f01c7bb94340da69a5c96a9b448493
c2038058 <gillj8@cardiff.ac.uk>
2021-12-10 12:05:06 +0000

    ShopDeleter fully logged, other classes changed to no longer use autowiring for attributes

--- src/main/java/com/example/clientproject/services/ShopDeleter.java
+++ src/main/java/com/example/clientproject/services/ShopDeleter.java
@@ -1,20 +1,31 @@
 package com.example.clientproject.services;
 
+import com.example.clientproject.service.LoggingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 @Service
 public class ShopDeleter {
-    @Autowired
     JdbcTemplate jdbc;
+    LoggingService loggingService;
 
     /**
      * @param shopId - the shopID of the shop that the stored procedure is going to delete
      */
-    public void deleteShop(Integer shopId){
+    public void deleteShop(Integer shopId, HttpSession session){
         String query = "CALL `mydb`.`deleteShop`('" + shopId + "');";
         jdbc.execute(query);
+        // Log the change
+        loggingService.logEvent(
+                "Deleted Shop",
+                session,
+                "Shop deleted with Shop Id: " + shopId +
+                        " in ShopDeleter.deleteShop()"
+        );
+
     }
 
 }
