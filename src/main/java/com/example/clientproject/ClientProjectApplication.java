45fa11b6d14f470c201d1d8a6462a4b7e6e84191
c2065864 <barnards3@cardiff.ac.uk>
2022-01-10 20:45:04 +0000

    Added more references

--- src/main/java/com/example/clientproject/ClientProjectApplication.java
+++ src/main/java/com/example/clientproject/ClientProjectApplication.java
@@ -42,11 +42,6 @@ public class ClientProjectApplication {
         return tomcat;
     }
 
-    /*
-We need to redirect from HTTP to HTTPS. Without SSL, this application used
-port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-redirected to HTTPS on 8443.
- */
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
